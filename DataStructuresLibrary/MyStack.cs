7b9f78863c6477a44e5cf78b8ea343a2bf2502dd
Szidor Patrik <patrikp987@gmail.com>
2024-01-31 15:40:23 +0100

    Made Items private

--- DataStructuresLibrary/MyStack.cs
+++ DataStructuresLibrary/MyStack.cs
@@ -4,7 +4,7 @@ public class MyStack<T>
 {
     private int _currentIndex;
     public int Size { get; private set; }
-    public T[] Items { get; private set; }
+    private readonly T[] _items;
 
     public int Count => _currentIndex + 1;
 
@@ -13,23 +13,23 @@ public class MyStack<T>
         if (size < 1) throw new ArgumentException("Size must be greater than 0");
 
         Size = size;
-        Items = new T[Size];
+        _items = new T[Size];
         _currentIndex = -1;
     }
 
     public void Push(T item)
     {
         if (_currentIndex + 1 >= Size) throw new ArgumentException("Stack Overflow");
-        Items[++_currentIndex] = item;
+        _items[++_currentIndex] = item;
     }
 
     public T Pop()
     {
         if (Count < 1) throw new ArgumentException("Stack Underflow");
-        return Items[_currentIndex--];
+        return _items[_currentIndex--];
     }
 
-    public T[] ToArray() => Items[..(_currentIndex + 1)];
+    public T[] ToArray() => _items[..(_currentIndex + 1)];
     
     public override string ToString() => "[" + string.Join(", ", ToArray()) + "]";
 }
