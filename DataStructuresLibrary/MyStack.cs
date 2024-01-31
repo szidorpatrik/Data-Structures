2a170733bd83d224f2bb0e039dbbf2b3519a7066
Szidor Patrik <patrikp987@gmail.com>
2024-01-31 15:04:45 +0100

    Created constructor

--- DataStructuresLibrary/MyStack.cs
+++ DataStructuresLibrary/MyStack.cs
@@ -2,5 +2,18 @@ namespace DataStructuresLibrary;
 
 public class MyStack<T>
 {
-    
+    private int _currentIndex;
+    public int Size { get; private set; }
+    public T[] Items { get; private set; }
+
+    public int Count => _currentIndex + 1;
+
+    public MyStack(int size)
+    {
+        if (size < 1) throw new ArgumentException("Size must be greater than 0");
+
+        Size = size;
+        Items = new T[Size];
+        _currentIndex = -1;
+    }
 }
