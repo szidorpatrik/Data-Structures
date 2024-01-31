069a48e462cd8843d4736026ae442a9002addd03
Szidor Patrik <patrikp987@gmail.com>
2024-01-31 15:37:56 +0100

    Implemented unit tests for Pop(), ToArray(), ToString()

--- DataStructuresTest/MyStackUnitTest.cs
+++ DataStructuresTest/MyStackUnitTest.cs
@@ -14,21 +14,74 @@ public class MyStackUnitTest
 
         Assert.ThrowsException<ArgumentException>(() =>
         {
-            MyStack<int> temp = new(0);
+            MyStack<int> _ = new(0);
         });
         Assert.ThrowsException<ArgumentException>(() =>
         {
-            MyStack<int> temp = new(-1);
+            MyStack<int> _ = new(-1);
         });
     }
-    
+
     [TestMethod]
-    public void Push()
+    public void OneLengthPushOneItem()
     {
         MyStack<int> stack = new(1);
         stack.Push(5);
         Assert.AreEqual(1, stack.Count);
-
+    }
+    
+    [TestMethod]
+    public void OneLengthPushTwoItems()
+    {
+        MyStack<int> stack = new(1);
+        stack.Push(5);
         Assert.ThrowsException<ArgumentException>(() => stack.Push(5));
+    }
+
+    [TestMethod]
+    public void Pop()
+    {
+        MyStack<int> stack = new(2);
+
+        stack.Push(5);
+        Assert.AreEqual(1, stack.Count);
+
+        stack.Push(3);
+        Assert.AreEqual(2, stack.Count);
+        
+        Assert.AreEqual(3, stack.Pop());
+        Assert.AreEqual(5, stack.Pop());
+    }
+    
+    [TestMethod]
+    public void PopFromEmptyStack()
+    {
+        MyStack<int> stack = new(1);
+        Assert.ThrowsException<ArgumentException>(() => stack.Pop());
+    }
+
+    [TestMethod]
+    public void ToArray()
+    {
+        MyStack<int> stack = new(2);
+        stack.Push(5);
+        stack.Push(3);
+        Assert.IsTrue(stack.ToArray().SequenceEqual(new [] { 5, 3 }));
+    }
+    
+    [TestMethod]
+    public void ToStringTwoItems()
+    {
+        MyStack<int> stack = new(2);
+        stack.Push(5);
+        stack.Push(3);
+        Assert.AreEqual("[5, 3]", stack.ToString());
+    }
+    
+    [TestMethod]
+    public void ToStringZeroItems()
+    {
+        MyStack<int> stack = new(2);
+        Assert.AreEqual("[]", stack.ToString());
     }
 }
