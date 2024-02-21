5f9cf12c17c86f423a9d0c30d9ad9749bcc9ee1b
Szidor Patrik <patrikp987@gmail.com>
2024-02-21 14:31:22 +0100

    Only unique values are added

--- DataStructuresLibrary/MySet.cs
+++ DataStructuresLibrary/MySet.cs
@@ -16,5 +16,34 @@ namespace DataStructuresLibrary
             }
         }
 
+        private Node? _root;
+        public int Count { get; private set; }
+
+        public void Add(T value)
+        {
+            bool isNewNode = false;
+            _root = AddRecursive(_root, value, ref isNewNode);
+            if (isNewNode) Count++;
+        }
+
+        private Node AddRecursive(Node? current, T value, ref bool isNewNode)
+        {
+            if (current is null)
+            {
+                isNewNode = true;
+                return new Node(value);
+            }
+
+            if (value.CompareTo(current.Value) == 0)
+                return current;
+
+            if (value.CompareTo(current.Value) < 0)
+                current.Left = AddRecursive(current.Left, value, ref isNewNode);
+
+            if (value.CompareTo(current.Value) > 0)
+                current.Right = AddRecursive(current.Right, value, ref isNewNode);
+
+            return current;
+        }
     }
 }
--- DataStructuresTest/MySetUnitTest.cs
+++ DataStructuresTest/MySetUnitTest.cs
@@ -1,7 +1,64 @@
+using DataStructuresLibrary;
+
 namespace DataStructuresTest;
 
 [TestClass]
 public class MySetUnitTest
 {
-    
+    private MySet<int> _set;
+
+    [TestInitialize]
+    public void InitializeTest()
+    {
+        _set = new MySet<int>();
+    }
+
+    [TestMethod]
+    public void Ctor()
+    {
+        Assert.AreEqual(0, _set.Count);
+    }
+
+    [TestMethod]
+    public void Length()
+    {
+        _set.Add(0);
+        Assert.AreEqual(1, _set.Count);
+
+        _set.Add(1);
+        Assert.AreEqual(2, _set.Count);
+
+        _set.Add(-1);
+        Assert.AreEqual(3, _set.Count);
+    }
+    [TestMethod]
+    [DataRow(new[] { 1, 3 })]
+    [DataRow(new[] { -1, 2, -3, 6 })]
+    [DataRow(new[] { -99, -2, 4889 })]
+    [DataRow(new[] { 0, 2, -3, 9, 8, 98, 897, -545, 18, -78 })]
+    public void AfterAddingUniqueValuesLengthIsCorrect(int[] items)
+    {
+        foreach (var item in items)
+            _set.Add(item);
+
+        Assert.AreEqual(items.Length, _set.Count);
+    }
+    [TestMethod]
+    [DataRow(1, 10)]
+    [DataRow(2, 20)]
+    [DataRow(0, -1)]
+    public void SameValueCanNotBeAdded(int val1, int val2)
+    {
+        _set.Add(val1);
+        Assert.AreEqual(1, _set.Count);
+        
+        _set.Add(val1);
+        Assert.AreEqual(1, _set.Count);
+
+        _set.Add(val2);
+        Assert.AreEqual(2, _set.Count);
+
+        _set.Add(val2);
+        Assert.AreEqual(2, _set.Count);
+    }
 }
