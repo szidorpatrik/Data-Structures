683b5b448eb010bd4e7d3a571f83803bd7f9540a
Szidor Patrik <patrikp987@gmail.com>
2024-02-21 14:29:15 +0100

    Created Node

--- DataStructuresLibrary/MySet.cs
+++ DataStructuresLibrary/MySet.cs
@@ -1,6 +1,20 @@
-namespace DataStructuresLibrary;
+using System.Numerics;
 
-public class MySet
+namespace DataStructuresLibrary
 {
-    
+    public class MySet<T> where T : IBinaryInteger<T>
+{
+        private sealed class Node
+        {
+            public Node? Left { get; set; }
+            public Node? Right { get; set; }
+            public T Value { get; }
+
+            public Node(T value)
+            {
+                Value = value;
+            }
+        }
+
+    }
 }
